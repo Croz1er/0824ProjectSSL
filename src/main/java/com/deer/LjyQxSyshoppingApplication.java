3951822eb073b659581864aafbe7f12a893228b7
LjyDeerWhale <norma_nono@163.com>
2020-08-24 12:51:32 +0800

    init

---
+++ src/main/java/com/deer/LjyQxSyshoppingApplication.java
@@ -0,0 +1,13 @@
+package com.deer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LjyQxSyshoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LjyQxSyshoppingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deer/LjyQxSyshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LjyQxSyshoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
